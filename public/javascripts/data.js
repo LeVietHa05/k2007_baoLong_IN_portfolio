68480e95042f5a8199e1492e747834678b9d4280
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-10-26 13:47:08 +0700

    update multiple content

---
+++ public/javascripts/data.js
@@ -0,0 +1,177 @@
+
+let graphicData = {
+    resonance: {
+        thumbnail: "./images/grad_img_1.png",
+        imgs: [
+            "./images/grad_resonance_1.png",
+            "./images/grad_resonance_2.png",
+            "./images/grad_resonance_3.png",
+            "./images/grad_resonance_4.png",
+            "./images/grad_resonance_5.png",
+            "./images/grad_resonance_6.png",
+            "./images/grad_resonance_7.png",
+            "./images/grad_resonance_8.png",
+        ],
+        overlay_text: "Resonance",
+        overlay_year: "Septemper 2024",
+        href: "/graphic/resonance"
+    },
+    song_vien: {
+        thumbnail: "./images/grad_img_2.png",
+        imgs: [
+            "./images/grad_song_vien_1.png",
+            "./images/grad_song_vien_2.png",
+            "./images/grad_song_vien_3.png",
+            "./images/grad_song_vien_4.png",
+            "./images/grad_song_vien_5.png",
+            "./images/grad_song_vien_6.png",
+            "./images/grad_song_vien_7.png",
+            "./images/grad_song_vien_8.png",
+            "./images/grad_song_vien_9.png",
+            "./images/grad_song_vien_10.png",
+            "./images/grad_song_vien_11.png",
+        ],
+        overlay_text: "Song Vien",
+        overlay_year: "October 2024",
+        href: "/graphic/song_vien"
+    },
+    rock_club: {
+        thumbnail: "./images/grad_img_3.png",
+        imgs: [
+            "./images/grad_rock_club_1.png",
+            "./images/grad_rock_club_2.png",
+            "./images/grad_rock_club_3.png",
+            "./images/grad_rock_club_4.png",
+            "./images/grad_rock_club_5.png",
+            "./images/grad_rock_club_6.png",
+            "./images/grad_rock_club_7.png",
+            "./images/grad_rock_club_8.png",
+            "./images/grad_rock_club_9.png",
+            "./images/grad_rock_club_10.png",
+            "./images/grad_rock_club_11.png",
+            "./images/grad_rock_club_12.png",
+            "./images/grad_rock_club_13.png",
+            "./images/grad_rock_club_14.png",
+            "./images/grad_rock_club_15.png",
+            "./images/grad_rock_club_16.png",
+            "./images/grad_rock_club_17.png",
+            "./images/grad_rock_club_18.png",
+            "./images/grad_rock_club_19.png",
+            "./images/grad_rock_club_20.png",
+            "./images/grad_rock_club_21.png",
+            "./images/grad_rock_club_22.png",
+        ],
+        overlay_text: "CNN Rock Club",
+        overlay_year: "October 2024",
+        href: "/graphic/rock_club"
+    },
+    vong_nga: {
+        thumbnail: "./images/grad_img_4.png",
+        imgs: [
+            "./images/grad_vong_nga_1.png",
+            "./images/grad_vong_nga_2.png",
+        ],
+        overlay_text: "Vong Nga",
+        overlay_year: "April 2024",
+        href: "/graphic/vong_nga"
+    },
+    grunge_collection: {
+        thumbnail: "./images/grad_img_5.png",
+        imgs: [
+            "./images/grad_grunge_collection_1.png",
+            "./images/grad_grunge_collection_2.png",
+            "./images/grad_grunge_collection_3.png",
+            "./images/grad_grunge_collection_4.png",
+            "./images/grad_grunge_collection_5.png",
+            "./images/grad_grunge_collection_6.png",
+            "./images/grad_grunge_collection_7.png",
+            "./images/grad_grunge_collection_8.png",
+        ],
+        overlay_text: "L@Mode Grunge Collection",
+        overlay_year: "April 2024",
+        href: "/graphic/grunge_collection"
+    },
+    brainstorming: {
+        thumbnail: "./images/grad_img_6.png",
+        imgs: [
+            "./images/grad_brainstorming_1.png",
+            "./images/grad_brainstorming_2.png",
+            "./images/grad_brainstorming_3.png",
+            "./images/grad_brainstorming_4.png",
+            "./images/grad_brainstorming_5.png",
+            "./images/grad_brainstorming_6.png",
+            "./images/grad_brainstorming_7.png",
+        ],
+        overlay_text: "3C Lab Brainstorming",
+        overlay_year: "April 2024",
+        href: "/graphic/brainstorming"
+    },
+    t_shirt_graphic: {
+        thumbnail: "./images/grad_img_7.png",
+        imgs: [
+            "./images/grad_t_shirt_graphic_1.png",
+            "./images/grad_t_shirt_graphic_2.png",
+            "./images/grad_t_shirt_graphic_3.png",
+        ],
+        overlay_text: "T-Shirt Graphic",
+        overlay_year: "Febuary 2024",
+        href: "/graphic/t_shirt_graphic"
+    },
+    international: {
+        thumbnail: "./images/grad_img_8.png",
+        imgs: [
+            "./images/grad_international_1.png",
+            "./images/grad_international_2.png",
+            "./images/grad_international_3.png",
+            "./images/grad_international_4.png",
+            "./images/grad_international_5.png",
+            "./images/grad_international_6.png",
+        ],
+        overlay_text: "International Vietnam Model United Nations",
+        overlay_year: "Febuary 2024",
+        href: "/graphic/international"
+    },
+    mi_chau: {
+        thumbnail: "./images/grad_img_9.png",
+        imgs: [
+            "./images/grad_mi_chau_1.png",
+            "./images/grad_mi_chau_2.png",
+            "./images/grad_mi_chau_3.png",
+            "./images/grad_mi_chau_4.png",
+        ],
+        overlay_text: "Mi Chau - A Cover By CRC",
+        overlay_year: "January 2024",
+        href: "/graphic/mi_chau"
+    },
+    hanoi_model: {
+        thumbnail: "./images/grad_img_10.png",
+        imgs: [
+            "./images/grad_hanoi_model_1.png",
+            "./images/grad_hanoi_model_2.png",
+            "./images/grad_hanoi_model_3.png",
+            "./images/grad_hanoi_model_4.png",
+            "./images/grad_hanoi_model_5.png",
+            "./images/grad_hanoi_model_6.png",
+        ],
+        overlay_text: "Hanoi Model United Nations",
+        overlay_year: "October 2023",
+        href: "/graphic/hanoi_model"
+    }
+}
+
+
+let photoshootData = {
+    bull_born: {
+        thumbnail: "./images/photo_img_1.png",
+        imgs: [
+            "./images/grad_bull_born_1.png",
+            "./images/grad_bull_born_2.png",
+            "./images/grad_bull_born_3.png",
+            "./images/grad_bull_born_4.png",
+            "./images/grad_bull_born_5.png",
+        ],
+        overlay_text: "Bull born under the sky",
+        overlay_year: "October 2024",
+        href: "/photoshoot/bull_born"
+    }
+}
